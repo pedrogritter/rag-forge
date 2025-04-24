ad6b905c47bd3d35800fdb254ee0ab32f1a84d1f
Pedro Gritter <pedro12345g@gmail.com>
2025-04-24 13:29:26 +0100

    fix: :wastebasket: comment out chat api callls

--- src/server/api/routers/chat.ts
+++ src/server/api/routers/chat.ts
@@ -1,32 +1,32 @@
-import { z } from "zod";
+// import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
-import { chats } from "@/server/db/schema";
-import { sql } from "drizzle-orm";
+// import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+// import { chats } from "@/server/db/schema";
+// import { sql } from "drizzle-orm";
 
-export const chatRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
+// export const chatRouter = createTRPCRouter({
+//   hello: publicProcedure
+//     .input(z.object({ text: z.string() }))
+//     .query(({ input }) => {
+//       return {
+//         greeting: `Hello ${input.text}`,
+//       };
+//     }),
 
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      await ctx.db.insert(chats).values({
-        name: input.name,
-      });
-    }),
+//   create: publicProcedure
+//     .input(z.object({ name: z.string().min(1) }))
+//     .mutation(async ({ ctx, input }) => {
+//       await ctx.db.insert(chats).values({
+//         name: input.name,
+//       });
+//     }),
 
-  getLatest: publicProcedure.query(async ({ ctx }) => {
-    const chat = await ctx.db
-      .select()
-      .from(chats)
-      .orderBy(sql`${chats.createdAt} DESC`)
-      .limit(1);
-    return chat[0] ?? null;
-  }),
-});
+//   getLatest: publicProcedure.query(async ({ ctx }) => {
+//     const chat = await ctx.db
+//       .select()
+//       .from(chats)
+//       .orderBy(sql`${chats.createdAt} DESC`)
+//       .limit(1);
+//     return chat[0] ?? null;
+//   }),
+// });
